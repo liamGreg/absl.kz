5138e84db93b07239e30dd9a53407b886b28ce62
serik <serikgreg@gmail.com>
2018-12-26 01:16:03 +0600

    add LESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp'),
     less = require('gulp-less'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    path = require('path'),
+    LessAutoprefix = require('less-plugin-autoprefix'),
+    autoprefix = new LessAutoprefix({browsers: ['last 2 versions']}),
+    sourcemaps = require('gulp-sourcemaps');
 
 function styles() {
-    return gulp.src('src/less/**/*.less')
-        .pipe(less())
+    return gulp.src(['src/less/**/*.less', '!src/less/**/_*.less'])
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')],
+            plugins: [autoprefix]
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/css'))
         .pipe(browserSync.stream());
 }
