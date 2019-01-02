4f4c7f4e871d1b51d3c2b0fa53ebc038e1c9fee8
serik <serikgreg@gmail.com>
2019-01-02 06:03:17 +0600

    main page

--- build/js/common.js
+++ build/js/common.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $('select:not(.no-selectize)').selectize();
 
     $('.header-slider').slick({
@@ -6,13 +6,75 @@ $(function() {
         dots: true,
         fade: true
     });
+
+    $('.thanks-slider').slick({
+        slidesToShow: 3,
+        appendArrows: '.thanks-slider-arrows',
+        prevArrow: '<div class="button button_primary button__next"></div>',
+        nextArrow: '<div class="button button_primary button__prev"></div>'
+    });
+
+    $('.thanks-doc-info__link a').magnificPopup({
+        type: 'image'
+    });
+});
+
+$(window).scroll(function () {
+    var wTop = $(this).scrollTop(), $header = $('header');
+
+    if (wTop > 250) {
+        if (!$header.hasClass('header_visible')) {
+            $header.show();
+            setTimeout(function () {
+                $header.addClass('header_visible');
+            }, 100);
+        }
+    } else {
+        if ($header.hasClass('header_visible')) {
+            $header.removeClass('header_visible');
+            setTimeout(function () {
+                $header.hide();
+            }, 100);
+        }
+    }
 });
 
 var $document = $(document);
 
+$document.on('click', '.csc-title', function () {
+    $(this).hide();
+    $('.csc-form').show().find('.csc-input').focus();
+});
+
+$document.on('blur', '.csc-input', function () {
+    if (!$(this).val()) {
+        $('.csc-form').hide();
+        $('.csc-title').show();
+    }
+});
+
+$document.on('click', '.drop__active, .drop-button', function (e) {
+    e.stopPropagation();
+});
+
+$document.on('click', 'body', function () {
+    $('.drop__active').removeClass('drop__active');
+    $('.drop-button__active').removeClass('drop-button__active');
+});
+
 $document.on('click', '.calc-button', function () {
     var $this = $(this);
 
-    $this.toggleClass('active');
-    $this.next().toggle();
+    $this.toggleClass('drop-button__active');
+    $this.next().toggleClass('drop__active');
+});
+
+$document.on('click', '.ss-list a', function (e) {
+    e.preventDefault();
+    var $li = $(this).parent(), ind = $li.index();
+
+    $li.addClass('active').siblings().removeClass('active');
+    $('.ss-content > div').eq(ind).addClass('active').siblings().removeClass('active');
+
+    return false;
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ const gulp = require('gulp'),
     path = require('path'),
     LessAutoprefix = require('less-plugin-autoprefix'),
     autoprefix = new LessAutoprefix({browsers: ['last 2 versions']}),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache');
 
 function styles() {
-    return gulp.src(['src/less/**/*.less', '!src/less/**/_*.less'])
+    return gulp.src(['src/less/**/*.less', '!src/less/**/_*.less', '!src/libs/*'])
         .pipe(sourcemaps.init())
         .pipe(less({
             paths: [path.join(__dirname, 'less', 'includes')],
@@ -16,6 +21,29 @@ function styles() {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/css'))
         .pipe(browserSync.stream());
+}
+
+function scripts() {
+    return gulp.src([
+        'src/js/jquery-3.3.1.min.js',
+        'src/js/selectize.min.js',
+        'src/js/slick.min.js',
+        'src/js/jquery.magnific-popup.min.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+}
+
+function images() {
+    return gulp.src('src/img/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant]
+        })))
+        .pipe(gulp.dest('build/img'));
 }
 
 function serve() {
@@ -28,6 +56,13 @@ function serve() {
     gulp.watch('src/less/**/*.less', styles);
     gulp.watch('build/*.html').on('change', browserSync.reload);
     gulp.watch('build/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('src/img/**/*', images);
 }
 
 gulp.task('serve', serve);
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+gulp.task('images', images);
+gulp.task('clear', function() {
+    return cache.clearAll();
+});
